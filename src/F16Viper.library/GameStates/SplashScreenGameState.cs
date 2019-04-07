86003d0a48c84ea7f3c2a6e7438698f3a5393604
Jarred Capellman <jcapellman@hotmail.com>
2019-04-07 15:57:27 -0400

    Added: Update override in the SplashScreenGameState class

--- src/F16Viper.library/GameStates/SplashScreenGameState.cs
+++ src/F16Viper.library/GameStates/SplashScreenGameState.cs
@@ -1,8 +1,22 @@
-﻿using F16Viper.library.GameStates.Base;
+﻿using System.Linq;
+
+using F16Viper.library.GameStates.Base;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace F16Viper.library.GameStates
 {
     public class SplashScreenGameState : BaseGameState
     {
+        public override void Update(GameTime gameTime, KeyboardState keyboardState, GamePadState gamePadState)
+        {
+            if (keyboardState.GetPressedKeys().Any())
+            {
+                ChangeState(typeof(MainMenuGameState));
+            }
+
+            base.Update(gameTime, keyboardState, gamePadState);
+        }
     }
 }
