63a8e7d1777d88eebe9a2d74b1af4ecbc1dbe1b4
Jarred Capellman <jcapellman@hotmail.com>
2019-04-07 15:26:42 -0400

    Added: Initial commit of the BaseRenderable class

---
+++ src/F16Viper.library/Renderables/Base/BaseRenderable.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace F16Viper.library.Renderables.Base
+{
+    public abstract class BaseRenderable
+    {
+        public void Render(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
