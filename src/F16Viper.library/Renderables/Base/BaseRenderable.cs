aa9d131cc5d5ce3885ec569623c1ed27350f64df
Jarred Capellman <jcapellman@hotmail.com>
2019-04-07 15:30:58 -0400

    Added: Update BaseRenderable class to support Update

--- src/F16Viper.library/Renderables/Base/BaseRenderable.cs
+++ src/F16Viper.library/Renderables/Base/BaseRenderable.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace F16Viper.library.Renderables.Base
 {
     public abstract class BaseRenderable
     {
         public void Render(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
         {
 
         }
