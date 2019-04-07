2efd00fecdfafaad5ac3c4c6ab5e08aba8e777a8
Jarred Capellman <jcapellman@hotmail.com>
2019-04-07 15:25:07 -0400

    Added: Initial commit of the BaseGameState class

---
+++ src/F16Viper.library/GameStates/Base/BaseGameState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+using F16Viper.library.Renderables.Base;
+
+using Microsoft.Xna.Framework.Graphics;
+
+namespace F16Viper.library.GameStates.Base
+{
+    public abstract class BaseGameState
+    {
+        private List<BaseRenderable> _renderables = new List<BaseRenderable>();
+
+        public virtual void Render(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+
+            foreach (var renderable in _renderables)
+            {
+                renderable.Render(spriteBatch);
+            }
+
+            spriteBatch.End();
+        }
+    }
+}
