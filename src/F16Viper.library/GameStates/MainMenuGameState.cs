bc66d9fd28f4514e18c0a24638a95ece90fdaf23
Jarred Capellman <jcapellman@hotmail.com>
2019-04-07 20:36:27 -0500

    Added: Initial work on the MainMenuGameState class setting the BackgroundTextureName

--- src/F16Viper.library/GameStates/MainMenuGameState.cs
+++ src/F16Viper.library/GameStates/MainMenuGameState.cs
@@ -1,8 +1,20 @@
 ﻿using F16Viper.library.GameStates.Base;
+using F16Viper.library.Renderables;
+
+using Microsoft.Xna.Framework.Content;
 
 namespace F16Viper.library.GameStates
 {
     public class MainMenuGameState : BaseGameState
     {
+        private const string BackgroundTextureName = "MenuBG";
+        
+        public override void LoadContent(ContentManager contentManager)
+        {
+            var background = new FSSplash(BackgroundTextureName);
+            background.LoadContent(contentManager);
+
+            AddRenderable(background);
+        }
     }
 }
