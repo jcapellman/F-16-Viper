13ce3c853581a5e058733aded4159286f56bcbac
Jarred Capellman <jcapellman@hotmail.com>
2019-04-07 20:37:14 -0500

    Added: Initial commit of the WindowContainer class

---
+++ src/F16Viper.library/Containers/WindowContainer.cs
@@ -0,0 +1,9 @@
+﻿namespace F16Viper.library.Containers
+{
+    public class WindowContainer
+    {
+        public int ResolutionX { get; set; }
+
+        public int ResolutionY { get; set; }
+    }
+}
