2e01d7958b5b39bd2f5492b0bb04ff0bffa8a21f
Jarred Capellman <jcapellman@hotmail.com>
2019-04-07 20:44:02 -0500

    Change: Airplane class now implements the BaseRenderable methods

--- src/F16Viper.library/Renderables/Airplane.cs
+++ src/F16Viper.library/Renderables/Airplane.cs
@@ -1,8 +1,28 @@
-﻿using F16Viper.library.Renderables.Base;
+﻿using F16Viper.library.Containers;
+using F16Viper.library.Renderables.Base;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace F16Viper.library.Renderables
 {
     public class Airplane : BaseRenderable
     {
+        public override void LoadContent(ContentManager contentManager)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Render(SpriteBatch spriteBatch, WindowContainer windowContainer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime, KeyboardState keyboardState, GamePadState gamePadState)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
