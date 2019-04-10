bdd41aaf3ee57368668a620c567a7817218c03cd
Jarred Capellman <jcapellman@hotmail.com>
2019-04-09 20:19:44 -0500

    Added: Skip through to the MainGameState class

--- src/F16Viper.library/GameStates/MainMenuGameState.cs
+++ src/F16Viper.library/GameStates/MainMenuGameState.cs
@@ -1,7 +1,11 @@
-﻿using F16Viper.library.GameStates.Base;
+﻿using System.Linq;
+    
+using F16Viper.library.GameStates.Base;
 using F16Viper.library.Renderables;
 
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
 
 namespace F16Viper.library.GameStates
 {
@@ -15,6 +19,16 @@ namespace F16Viper.library.GameStates
             background.LoadContent(contentManager);
 
             AddRenderable(background);
+        }
+
+        public override void Update(GameTime gameTime, KeyboardState keyboardState, GamePadState gamePadState)
+        {
+            if (keyboardState.GetPressedKeys().Any())
+            {
+                ChangeState(typeof(MainGameState));
+            }
+
+            base.Update(gameTime, keyboardState, gamePadState);
         }
     }
 }
