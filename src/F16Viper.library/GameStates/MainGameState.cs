86ac9a12b2838030ac11b05515aef31da6b9c0a5
Jarred Capellman <jcapellman@hotmail.com>
2019-04-09 20:19:10 -0500

    Added: Initial work on the MainGameState class

---
+++ src/F16Viper.library/GameStates/MainGameState.cs
@@ -0,0 +1,18 @@
+﻿using F16Viper.library.GameStates.Base;
+using F16Viper.library.Renderables;
+
+using Microsoft.Xna.Framework.Content;
+
+namespace F16Viper.library.GameStates
+{
+    public class MainGameState : BaseGameState
+    {
+        public override void LoadContent(ContentManager contentManager)
+        {
+            var airplane = new Airplane("F18");
+            airplane.LoadContent(contentManager);
+
+            AddRenderable(airplane);
+        }
+    }
+}
