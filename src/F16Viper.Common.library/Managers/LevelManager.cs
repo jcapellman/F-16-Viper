fee146be1cc4bb67c218a14b275601f772bc6bbb
Jarred Capellman <jcapellman@hotmail.com>
2019-04-14 09:12:18 -0500

    Added: Initial work on the LevelManager class

---
+++ src/F16Viper.Common.library/Managers/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+using F16Viper.Common.library.JSONObjects;
+
+using Newtonsoft.Json;
+
+namespace F16Viper.Common.library.Managers
+{
+    public class LevelManager
+    {
+        public static LevelJSON LoadLevel(string levelName) => JsonConvert.DeserializeObject<LevelJSON>(File.ReadAllText($"Levels/{levelName}.json"));
+    }
+}
