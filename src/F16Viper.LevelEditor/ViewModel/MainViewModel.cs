f8be6bea300c9f3d8954b456b04ee7e5ea742b79
Jarred Capellman <jcapellman@hotmail.com>
2019-04-14 10:35:15 -0500

    Added: Tile View in the Level Editor #2

--- src/F16Viper.LevelEditor/ViewModel/MainViewModel.cs
+++ src/F16Viper.LevelEditor/ViewModel/MainViewModel.cs
@@ -1,5 +1,10 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Windows.Forms;
+using System.Windows.Media.Imaging;
 
 using F16Viper.Common.library.JSONObjects;
 using F16Viper.Common.library.Managers;
@@ -8,14 +13,42 @@ namespace F16Viper.LevelEditor.ViewModel
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
         private string _currentFileName = string.Empty;
         private LevelJSON _currentLevel;
+
+        private ObservableCollection<string> _tiles;
+
+        public ObservableCollection<string> Tiles
+        {
+            get => _tiles;
+
+            set
+            {
+                _tiles = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // TODO: Make this configurable
+        private const string TILE_FILE_PATH = "../../../../../assets/gfx/Tiles/";
 
         public MainViewModel()
         {
             _currentLevel = new LevelJSON();
+
+            LoadTiles();
+        }
+
+        private void LoadTiles()
+        {
+            Tiles = new ObservableCollection<String>();
+
+            var files = Directory.GetFiles(TILE_FILE_PATH);
+
+            foreach (var file in files)
+            {
+                Tiles.Add(new FileInfo(file).FullName);
+            }
         }
 
         internal void NewLevel()
@@ -60,6 +93,13 @@ namespace F16Viper.LevelEditor.ViewModel
             }
 
             LevelManager.SaveLevel(_currentFileName, _currentLevel);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
