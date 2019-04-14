f7fad2857148b891a502936a4c98f21cd9ee5bb9
Jarred Capellman <jcapellman@hotmail.com>
2019-04-14 10:03:56 -0500

    Added: Opening and Saving Maps #4

--- src/F16Viper.LevelEditor/MainWindow.xaml.cs
+++ src/F16Viper.LevelEditor/MainWindow.xaml.cs
@@ -1,32 +1,38 @@
 ﻿using System.Windows;
 
+using F16Viper.LevelEditor.ViewModel;
+
 namespace F16Viper.LevelEditor
 {
     public partial class MainWindow : Window
     {
+        private MainViewModel ViewModel => (MainViewModel)DataContext;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainViewModel();
         }
 
-        private void mnuItemExit_Click(object sender, RoutedEventArgs e)
+        private void MnuItemExit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
 
-        private void mnuItemNew_Click(object sender, RoutedEventArgs e)
+        private void MnuItemNew_Click(object sender, RoutedEventArgs e)
         {
-
+            ViewModel.NewLevel();
         }
 
-        private void mnuItemOpen_Click(object sender, RoutedEventArgs e)
+        private void MnuItemOpen_Click(object sender, RoutedEventArgs e)
         {
-
+            ViewModel.LoadLevel();
         }
 
-        private void mnuItemSave_Click(object sender, RoutedEventArgs e)
+        private void MnuItemSave_Click(object sender, RoutedEventArgs e)
         {
-
+            ViewModel.SaveLevel();
         }
     }
 }
---
+++ src/F16Viper.LevelEditor/ViewModel/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+using F16Viper.Common.library.JSONObjects;
+using F16Viper.Common.library.Managers;
+
+namespace F16Viper.LevelEditor.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _currentFileName = string.Empty;
+        private LevelJSON _currentLevel;
+
+        public MainViewModel()
+        {
+            _currentLevel = new LevelJSON();
+        }
+
+        internal void NewLevel()
+        {
+            _currentFileName = string.Empty;
+
+            _currentLevel = new LevelJSON();
+        }
+
+        internal void LoadLevel()
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "level files (*.map)|*.map";
+
+                if (ofd.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+
+                _currentFileName = ofd.FileName;
+
+                _currentLevel = LevelManager.LoadLevelFile(_currentFileName);
+            }
+        }
+
+        internal void SaveLevel()
+        {
+            if (string.IsNullOrEmpty(_currentFileName))
+            {
+                using (var sfd = new SaveFileDialog())
+                {
+                    sfd.Filter = "level files (*.map)|*.map";
+
+                    if (sfd.ShowDialog() != DialogResult.OK)
+                    {
+                        return;
+                    }
+
+                    _currentFileName = sfd.FileName;
+                }
+            }
+
+            LevelManager.SaveLevel(_currentFileName, _currentLevel);
+        }
+    }
+}
