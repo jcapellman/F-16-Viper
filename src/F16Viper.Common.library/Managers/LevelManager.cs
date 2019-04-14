abe54ffcece975e7c988d147f0970de2772e0c1b
Jarred Capellman <jcapellman@hotmail.com>
2019-04-14 10:03:24 -0500

    Added: Save/LoadLevel/LoadLevelFile methods in the LevelManager class

--- src/F16Viper.Common.library/Managers/LevelManager.cs
+++ src/F16Viper.Common.library/Managers/LevelManager.cs
@@ -8,6 +8,17 @@ namespace F16Viper.Common.library.Managers
 {
     public class LevelManager
     {
-        public static LevelJSON LoadLevel(string levelName) => JsonConvert.DeserializeObject<LevelJSON>(File.ReadAllText($"Levels/{levelName}.json"));
+        public static LevelJSON LoadLevelFile(string fileName) => JsonConvert.DeserializeObject<LevelJSON>(File.ReadAllText(fileName));
+
+        public static LevelJSON LoadLevel(string levelName) => LoadLevelFile($"Levels/{levelName}.map");
+
+        public static bool SaveLevel(string fileName, LevelJSON level)
+        {
+            var json = JsonConvert.SerializeObject(level);
+
+            File.WriteAllText(fileName, json);
+
+            return true;
+        }
     }
 }
