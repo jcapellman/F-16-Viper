ede765b812b55b22e8ea07acd478777eca769ae5
Jarred Capellman <jcapellman@hotmail.com>
2019-04-14 09:11:19 -0500

    Change: LevelJSON now lives in the Common Library

---
+++ src/F16Viper.Common.library/JSONObjects/LevelJSON.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace F16Viper.Common.library.JSONObjects
+{
+    public class LevelJSON
+    {
+        public List<string> Textures { get; set; }
+
+        public List<int> Tiles { get; set; }
+    }
+}
